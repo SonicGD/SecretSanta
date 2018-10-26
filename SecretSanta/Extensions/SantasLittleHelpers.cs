30abd2d261b0c9feac8afc9102c0a3a93647f5f1
SonicGD <sonicgd@gmail.com>
2018-10-26 19:27:55 +0500

    More fixes

--- SecretSanta/Extensions/SantasLittleHelpers.cs
+++ SecretSanta/Extensions/SantasLittleHelpers.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 
-namespace SecretSanta.Extentions
+namespace SecretSanta.Extensions
 {
     public static class SantasLittleHelpers
     {
--- SecretSanta/SecretSanta.cs
+++ SecretSanta/SecretSanta.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using SecretSanta.Extentions;
+using SecretSanta.Extensions;
 
 namespace SecretSanta
 {
